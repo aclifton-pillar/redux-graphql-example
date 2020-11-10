b9ff6cd58b720664aa0858aafc9607002f77e167
Anthony Clifton <anthony.s.clifton@accenture.com>
2020-11-10 10:38:26 -0600

    completed tutorial in start except for authentication

---
+++ start/server/src/schema.js
@@ -0,0 +1,63 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Launch {
+      id: ID!
+      site: String
+      mission: Mission
+      rocket: Rocket
+      isBooked: Boolean!
+    }
+
+    type Rocket {
+      id: ID!
+      name: String
+      type: String
+    }
+    
+    type User {
+      id: ID!
+      email: String!
+      trips: [Launch]!
+    }
+    
+    type Mission {
+      name: String
+      missionPatch(size: PatchSize): String
+    }
+    
+    enum PatchSize {
+      SMALL
+      LARGE
+    }
+    
+    type Query {
+      launches( # replace the current launches query with this one.
+        pageSize: Int
+        after: String
+      ): LaunchConnection!
+      launch(id: ID!): Launch
+      me: User
+    }
+
+    type Mutation {
+      bookTrips(launchIds: [ID]!): TripUpdateResponse!
+      cancelTrip(launchId: ID!): TripUpdateResponse!
+      login(email: String): String # login token
+    }
+
+    type TripUpdateResponse {
+      success: Boolean!
+      message: String
+      launches: [Launch]
+    }
+    
+    type LaunchConnection {
+      cursor: String!
+      hasMore: Boolean!
+      launches: [Launch]!
+    }
+
+`;
+
+module.exports = typeDefs;
